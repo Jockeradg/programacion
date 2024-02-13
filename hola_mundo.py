dc727ba17e607e8e18bd130bd966102a28a57922
Jockeradg <adrieldiego05@gmail.com>
2024-02-13 09:58:31 +0100

    Archivo plantilla
    Se crea un archivo plantilla para una elaboración más sencilla del resto de archivos.

--- hola_mundo.py
+++
@@ -1,5 +0,0 @@
-"""
-Este archivo contiene un programa simple que imprime un saludo en español.
-"""
-
-print("Hola, ¿qué tal?")
---
+++ plantilla.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Descripción: Este es un ejemplo de un archivo de Python
+
+@autor: Adriel Diego
+@date: 
+"""
