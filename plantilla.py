d9143ca53665a04ee9854ec42f71aaaa938fe13a
Jockeradg <adrieldiego05@gmail.com>
2024-02-13 10:04:22 +0100

    Update plantilla.py
    Se forma los codigos iniciales, con la implementación de name==main para que se ejecute como programa y no como modulo.

--- plantilla.py
+++ plantilla.py
@@ -4,4 +4,7 @@ Descripción: Este es un ejemplo de un archivo de Python
 
 @autor: Adriel Diego
 @date: 
+
 """
+if __name__ == "__main__": # Si se ejecuta este archivo, se ejecutará lo siguiente, pero si se importa, no se ejecutará
+    print("Hola mundo!")
