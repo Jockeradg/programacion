368090edac460d8782eae2b566b216fae87479c7
Jockeradg <adrieldiego05@gmail.com>
2024-02-20 22:49:14 +0100

    Tipo de variables
    Actualizar el tipo de variable de cada una de las variables del script

--- Ejercicios/Practica_2/ejercicio1.py
+++ Ejercicios/Practica_2/ejercicio1.py
@@ -12,15 +12,14 @@ import numpy as np # Importar la librería numpy para resolver el sistema de ecu
 if __name__ == "__main__": # Si se ejecuta este archivo, se ejecutará lo siguiente, pero si se importa como paquete, no se ejecutará
 
  # Definir las ecuaciones
- 
  # Coeficientes de las incógnitas
- coeficientes = np.array([[1, -2], [4, 2]])
+ coeficientes:list = np.array([[1, -2], [4, 2]])
 
  # Términos independientes
- terminos_independientes = np.array([3, 2])
+ terminos_independientes:list = np.array([3, 2])
 
  # Resolver el sistema de ecuaciones
- solucion = np.linalg.solve(coeficientes, terminos_independientes)
+ solucion:list = np.linalg.solve(coeficientes, terminos_independientes)
 
  # Imprimir las soluciones
  print("El valor para x es:", solucion[0])
