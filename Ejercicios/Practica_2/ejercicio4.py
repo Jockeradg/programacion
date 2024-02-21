dad4d2fea5ffb8782c3a147e37386ee0b803e519
Jockeradg <adrieldiego05@gmail.com>
2024-02-21 14:13:18 +0100

    Update ejercicio4.py
    Finalización de ejercicio 4

--- Ejercicios/Practica_2/ejercicio4.py
+++ Ejercicios/Practica_2/ejercicio4.py
@@ -1,13 +1,42 @@
-print("      *      ")
-print("     ***     ")
-print("    *****    ")
-print("   *******   ")
-print("  *********  ")
-print(" ***********")
-print("** *** *** **")
-print("** *** *** **")
-print("*************")
-print("*************")
-print("*****  ******")
-print("*****  ******")
-print("*****  ******")
+# -*- coding: utf-8 -*-
+"""
+Descripción: Este código imprime una casa con un tejado, ventanas y una puerta
+y puedes hacer uso de las funciones para imprimir las alturas de las casas
+
+@autor: Adriel Diego
+@date: 21/02/2024
+
+"""
+
+def tejado(): # Defino la función para imprimir el tejado
+    print("      *      ")
+    print("     ***     ")
+    print("    *****    ")
+    print("   *******   ")
+    print("  *********  ")
+    print(" ***********")
+    return None
+
+def ventanas(): # Defino la función para imprimir las ventanas
+    print("*************")
+    print("** *** *** **")
+    print("** *** *** **")
+    print("*************")
+    print("*************")
+    return None
+
+def puerta(): # Defino la función para imprimir la puerta
+    print("*****  ******")
+    print("*****  ******")
+    print("*****  ******")
+    return None
+
+tejado()
+ventanas()
+ventanas()
+ventanas()
+puerta()
+
+"""
+Invoco las funciones para imprimir la casa
+"""
