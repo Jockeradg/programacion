8c876a5bf7067b9771cff528230f3ffff89cb254
Jockeradg <adrieldiego05@gmail.com>
2024-02-21 13:52:27 +0100

    Update
    Mejora de documentacion y ejercicio 4

--- Ejercicios/Practica_2/ejercicio3.py
+++ Ejercicios/Practica_2/ejercicio3.py
@@ -6,17 +6,17 @@ Descripción: Este es un ejemplo de un archivo de Python
 @date: 21/02/2024
 """
 
-a1 = 1
-b1 = -2
-c1 = 3
-a2 = 3.5
-b2 = -4
-c2 = 8.5
+a1:float = 1
+b1:float = -2
+c1:float = 3
+a2:float = 3.5
+b2:float = -4
+c2:float = 8.5
 """
 Declaro las variables con los valores de los coeficientes de las incógnitas y los términos independientes
 """
-y = (c1*a2-a1*c2)/(a2*b1-a1*b2)
-x = (c2-b2*y)/a2
+y:float = (c1*a2-a1*c2)/(a2*b1-a1*b2)
+x:float = (c2-b2*y)/a2
 """
 Guardo en las variables x e y los valores de las soluciones del sistema de ecuaciones mediante el método de sustitución
 """
