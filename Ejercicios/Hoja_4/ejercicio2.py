a832fb88306592ed00c685e1d701a912070906c7
Jockeradg <adrieldiego05@gmail.com>
2024-03-26 12:10:48 +0100

    Update ejercicio2.py
    Corrección del código en clase

--- Ejercicios/Hoja_4/ejercicio2.py
+++ Ejercicios/Hoja_4/ejercicio2.py
@@ -1,15 +1,40 @@
 class Hipoteca:
-    def __init__(self, capital, interes_anual, numero_mensualidades):
-        self.capital = capital
-        self.interes_anual = interes_anual
-        self.numero_mensualidades = numero_mensualidades
+    """
+    Clase que representa una hipoteca.
+    Atributos:
+        __porcentaje_tipo_interes_anual (float): Porcentaje del tipo de interés anual.
+        __capital (float): Capital de la hipoteca.
+        __numero_mensualidades (int): Número de mensualidades.
+        __entidad_bancaria (str): Nombre de la entidad bancaria.
+    """
 
-    def calcular_pago_mensual(self):
-        interes_mensual = self.interes_anual / 12
-        pago_mensual = (self.capital * interes_mensual) / (1 - (1 + interes_mensual) ** -self.numero_mensualidades)
-        return pago_mensual
+    def __init__(self, porcentaje_tipo_interes_anual: float, capital: float, 
+                 numero_mensualidades: int, entidad_bancaria: str):
+        """
+        Genera un objeto con información necesaria para calcular una hipoteca.
+        Args:
+            porcentaje_tipo_interes_anual (float): Porcentaje del tipo de interés anual.
+            capital (float): Capital de la hipoteca.
+            numero_mensualidades (int): Número de mensualidades.
+            entidad_bancaria (str): Nombre de la entidad bancaria.
+        """
+        self.__tipo_interes_anual = porcentaje_tipo_interes_anual / 100
+        self.__capital = capital
+        self.__numero_mensualidades = numero_mensualidades
+        self.__entidad_bancaria = entidad_bancaria
+
+    def calcular_pago_mensual(self)->float:
+        """
+        Calcula el pago mensual de la hipoteca.
+
+        Returns:
+            (float): Devuelve el pago mensual de la hipoteca.
+        """
+        tipo_interes_mensual = self.interes_anual / 12
+        return (self.capital * tipo_interes_mensual) / (1 - (1 + tipo_interes_mensual) ** -self.__numero_mensualidades)
+        
 
 # Example usage
-hipoteca = Hipoteca(100000, 0.05, 12)
+hipoteca = Hipoteca(100000, 0.05, 12, "Santander")
 pago_mensual = hipoteca.calcular_pago_mensual()
 print(f"El pago mensual de la hipoteca es: {pago_mensual} euros")
