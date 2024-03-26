d4fc3f3f8abb6cb865812d5409db7e6bcb07c85d
Jockeradg <adrieldiego05@gmail.com>
2024-03-26 11:57:16 +0100

    Update hipoteca.py
    Correcciones de documentación de código y de la creación de clases.

--- Ejercicios/Hoja_4/hipoteca.py
+++ Ejercicios/Hoja_4/hipoteca.py
@@ -1,14 +1,24 @@
 class Hipoteca:
-    def __init__(self, porcentaje_tipo_interes_anual: float, capital: float, numero_mensualidades: int, entidad_bancaria: str):
+    """
+    Clase que representa una hipoteca.
+    Atributos:
+        __porcentaje_tipo_interes_anual (float): Porcentaje del tipo de interés anual.
+        __capital (float): Capital de la hipoteca.
+        __numero_mensualidades (int): Número de mensualidades.
+        __entidad_bancaria (str): Nombre de la entidad bancaria.
+    """
+
+    def __init__(self, porcentaje_tipo_interes_anual: float, capital: float, 
+                 numero_mensualidades: int, entidad_bancaria: str):
         """
-        Constructor de la clase Hipoteca.
+        Genera un objeto con información necesaria para calcular una hipoteca.
         Args:
             porcentaje_tipo_interes_anual (float): Porcentaje del tipo de interés anual.
             capital (float): Capital de la hipoteca.
             numero_mensualidades (int): Número de mensualidades.
             entidad_bancaria (str): Nombre de la entidad bancaria.
         """
-        self.tipo_interes_anual = porcentaje_tipo_interes_anual / 100
-        self.capital = capital
-        self.numero_mensualidades = numero_mensualidades
-        self.entidad_bancaria = entidad_bancaria
+        self.__tipo_interes_anual = porcentaje_tipo_interes_anual / 100
+        self.__capital = capital
+        self.__numero_mensualidades = numero_mensualidades
+        self.__entidad_bancaria = entidad_bancaria
